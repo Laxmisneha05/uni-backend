32f9faf9349279e578db752afaaa3b7f3f6cfe38
Laxmisneha05 <laxmisnehac@gmail.com>
2025-03-21 12:57:00 +0530

    added pica routes and services

--- src/routes/pcacsRoutes.js
+++ src/routes/pcacsRoutes.js
@@ -5,19 +5,4 @@ export default async function (fastify, options) {
     const students = await getPCACSStudents();
     reply.send(students);
   });
-  fastify.get('/pce/students/:id', async (req, reply) => {
-      return getPCEStudentById(req.params.id);
-    });
-  
-    fastify.post('/pce/students', async (req, reply) => {
-      return addPCEStudent(req.body);
-    }); 
-  
-    fastify.put('/pce/students/:id', async (req, reply) => {
-      return updatePCEStudent(req.params.id, req.body);
-    });
-  
-    fastify.delete('/pce/students/:id', async (req, reply) => {
-      return deletePCEStudent(req.params.id);
-    });
 }
--- src/routes/picaRoutes.js
+++ src/routes/picaRoutes.js
@@ -1,8 +1,39 @@
-import { getPICAStudents } from '../services/picaService.js';
+import { 
+  getPICAStudents, 
+  getPICAStudentById, 
+  addPICAStudent, 
+  updatePICAStudent, 
+  deletePICAStudent 
+} from '../services/picaService.js';
 
 export default async function (fastify, options) {
   fastify.get('/pica/students', async (req, reply) => {
     const students = await getPICAStudents();
     reply.send(students);
+  });
+
+  fastify.get('/pica/students/:id', async (req, reply) => {
+    const { id } = req.params;
+    const student = await getPICAStudentById(id);
+    reply.send(student);
+  });
+
+  fastify.post('/pica/students', async (req, reply) => {
+    const studentData = req.body;
+    const newStudent = await addPICAStudent(studentData);
+    reply.code(201).send(newStudent);
+  });
+
+  fastify.put('/pica/students/:id', async (req, reply) => {
+    const { id } = req.params;
+    const studentData = req.body;
+    const updatedStudent = await updatePICAStudent(id, studentData);
+    reply.send(updatedStudent);
+  });
+
+  fastify.delete('/pica/students/:id', async (req, reply) => {
+    const { id } = req.params;
+    const deletedStudent = await deletePICAStudent(id);
+    reply.send(deletedStudent);
   });
 }
