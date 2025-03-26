4cdfbabd425e8b909a1fc0b66a1696dc9715ba08
Laxmisneha05 <laxmisnehac@gmail.com>
2025-03-26 14:57:14 +0530

    add validation folder

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -1,7 +1,7 @@
 import { getPrograms } from '../services/publicService.js';
 
 export default async function (fastify, options) {
-  fastify.get('/programs', async (req, reply) => {
+  fastify.get('/users', async (req, reply) => {
     const programs = await getPrograms();
     reply.send(programs);
   });
